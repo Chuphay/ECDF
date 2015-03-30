85a8facf48a09473c7063a580fe083b1a8eda221
Chuphay <chuphay@gmail.com>
2015-03-30 13:32:39 -0700

    time to check how python 2.7 deals

---
+++ ecdf/ecdf.py
@@ -0,0 +1,65 @@
+"""ECDF
+Suppose you have multiple text files, each containing lines of the form:
+student_id,course_name,school_name,test_date,test_score
+
+An example valid line could look like the following (note that the files will not have headers):
+9812345,”Algebra”,”Port Chester University”,2015-03-17,75.5
+
+Write a program to compute the empirical cumulative distribution function of the average test score of students who attended a particular school, which is specified as a parameter to the program.
+
+An example invocation of the program would be:
+python ecdf.py --school “Port Chester University” input_file1.csv input_file2.csv
+
+The expected output from invoking such a program would be of the following form (use a single tab character to delimit field values, and display the results to standard output):
+
+Port Chester University students
+
+percentile    mean_test_score
+1                  0
+2                  0
+3                  5.25
+…                …
+100              100
+
+"""
+
+import sys
+
+class InvalidArgumentError(ValueError): pass
+class FileError(IOError): pass
+
+def parseArg(argv):
+    """Parses the command line arguments. Checks for errors and returns a tuple (school, [files, to, search])"""
+
+    if not isinstance(argv, list): 
+        """Check to make sure argv is a list"""
+        raise InvalidArgumentError('Must pass a list to parseArg.')
+
+    if(len(argv)<4):
+        """All arguments must have the form ['ecdf.py', '--school', '"name_of_university"', input_file1.csv]"""
+        raise InvalidArgumentError("""This program must be run with the following format: \npython ecdf.py --school “Port Chester University” file1.csv file2.csv""")
+    
+    if(argv[1] != "--school"):
+        """This program only has one option: to aggregate according to school"""
+        raise InvalidArgumentError("""This program must be run with the following format: \npython ecdf.py --school “Port Chester University” file1.csv file2.csv""")
+
+    return (argv[2], argv[3:])
+
+
+
+
+
+def getData(university, files):
+    for this_file in files:
+        try:
+            f = open(this_file)
+            for line in f:
+                pass
+        except (FileNotFoundError, IOError):
+            error_string = "Could not open "+ str(this_file)+". Check that the path is correct."
+            raise FileError (error_string) 
+
+
+if __name__ == '__main__':
+    school, files = parseArg(sys.argv)
+    getData(school, files)
---
+++ tests/ecdf_tests.py
@@ -0,0 +1,71 @@
+"""Unit tests for ECDF, an empirical cumulative distribution function finder. Since I am using a test-first development strategy, there will be some comments in this script that describe my thinking process as I build the package.
+
+I have decided to have four main functions in the program: parseArg, getData, makeECDF, printECDF. ParseArg will take the arguments provided by the user and parse them to make sure that they are valid. GetData will then take those arguments and scan through the selected files for the relevant information. MakeECDF will then take that data and return the ECDF in a compact form. PrintECDF will take the ECDF provided by MakeECDF and return a string that is in the correct format."""
+
+from ecdf.ecdf import *
+import unittest
+
+scipy_imported = False #this will check to see if I loaded ECDF from the scipy stack
+try:
+    import numpy as np
+    from statsmodels.distributions.empirical_distribution import ECDF
+    scipy_imported = True
+except ImportError:
+    print("Could not load statsmodels. Will not be able to test all aspects of ecdf")
+
+
+
+class TestParseArg(unittest.TestCase):
+    """I will divide my tests into two parts, first inputs that should return errors and then, secondly, inputs that should return correct output"""
+
+    def test_wrong_type_of_argument(self):
+        """testing for non-array arguments"""
+        self.assertRaises(InvalidArgumentError, parseArg, "string")
+        self.assertRaises(InvalidArgumentError, parseArg, True)
+        self.assertRaises(InvalidArgumentError, parseArg, 3.14)
+
+    def test_no_arguments(self):
+        """parseArg should raise an exception if there were no arguments"""
+        self.assertRaises(InvalidArgumentError, parseArg, [])
+
+    def test_too_few_arguments(self):
+        """parseArg should raise an exception if there is not enough arguments"""
+        self.assertRaises(InvalidArgumentError, parseArg, ["one"])
+        self.assertRaises(InvalidArgumentError, parseArg, ["one", "two"])
+        self.assertRaises(InvalidArgumentError, parseArg, ["one", "two","three"])
+        self.assertRaises(InvalidArgumentError, parseArg, ["ecdf.py", "--school", "Port Chester University"])
+
+    def test_wrong_arguments(self):
+        """If the arguments are not in the correct order, an Error should be thrown"""
+        self.assertRaises(InvalidArgumentError, parseArg, ["ecdf.py",'file1.csv',"Port Chester University", "--school"])
+
+    #Above were the tests for the bad inputs, now we will test the good inputs
+
+    def test_one_file(self):
+        """testing one csv file"""
+        result = parseArg(["ecdf.py", "--school","Port Chester University",'file1.csv']) 
+        self.assertEqual(("Port Chester University", ["file1.csv"]), result) 
+
+    def test_multiple_files(self):
+        """testing multiple csv files"""
+        result = parseArg(["ecdf.py", "--school","Port Chester University",'file1.csv',"file2.csv"]) 
+        self.assertEqual(("Port Chester University", ["file1.csv", "file2.csv"]), result)
+        result = parseArg(["ecdf.py", "--school","Port Chester University",'file1.csv',"file2.csv", "file3.txt"]) 
+        self.assertEqual(("Port Chester University", ["file1.csv", "file2.csv", "file3.txt"]), result)
+
+class TestGetData(unittest.TestCase):
+    """I will divide my tests into two parts, first inputs that should return errors and then, secondly, inputs that should return correct output"""
+
+    def test_no_file_present(self):
+        """test to make sure that the file asked for is actually found"""
+        self.assertRaises(FileError, getData, "ABC University", ["file1.csv", "not_a_file.csv", "file2.csv"])
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
